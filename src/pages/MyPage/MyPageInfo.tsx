a212380e2c372e244f91ec3684d33cb326be9236
Yujin Im <jina0605@gmail.com>
2022-02-22 03:59:12 +0900

    MyPageInfo mobile publishing

--- src/pages/MyPage/MyPageInfo.tsx
+++ src/pages/MyPage/MyPageInfo.tsx
@@ -12,11 +12,21 @@ export const MyPageInfo = () => {
 
   return (
     <div className="w-full">
-      <div className="flex bg-brand-3 py-1 px-8 space-x-2 my-5">
+      <p className="text-xs md:hidden mt-4 ml-6">
+        <span className="text-gray-500">
+          마이페이지 {">"} 내정보관리 {">"}{" "}
+        </span>
+        <span>
+          {pathname.startsWith("/mypage/info/edit")
+            ? "개인정보수정"
+            : "회원탈퇴"}
+        </span>
+      </p>
+      <div className="flex bg-brand-3 py-1 px-6 md:px-8 space-x-2 my-5">
         {tabs.map(({ text, route }) => (
           <Link
             to={route}
-            className={`rounded text-sm px-2 py-0.5 ${
+            className={`rounded-sm text-sm px-2 py-0.5 ${
               pathname === `/mypage/info/${route}`
                 ? "bg-brand-1 text-white"
                 : "bg-white"
